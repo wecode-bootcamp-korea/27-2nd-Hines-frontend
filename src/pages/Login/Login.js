e254029fcce343e63a8dcde8055c7677ddd2a39a
leejihyun <ix219@naver.com>
2021-12-22 12:17:55 +0900

    Modify : 로그인 기능

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,33 +5,20 @@ const { Kakao } = window;
 
 const kakaoLoginClickHandler = () => {
   Kakao.Auth.login({
-    // <고급 팝업방식으로 로그인>
-    // 카카오 로그인 동의 화면을 팝업으로 띄우거나 클라이언트에서 모든 인증 처리를 하고 싶은 경우 Kakao.Auth.login 함수를 사용할 수 있다.
-    // 클릭 이벤트 핸들러에서 함수를 호출하면 카카오 로그인 동의 화면을 띄울 수 있으며, 동의 화면을 통해 사용자로부터
-    // 사용자 정보 및 기능 활용 동의를 받을 수 있다.
-    // 함수 호출 시 팝업으로 카카오 로그인 동의 화면이 표시된다.
-    // 로그인 요청 결과 토근이 발급되며 이 토큰은 SDK 내부적으로 사용되고 있기 때문에 별도의 처리가 필요하지 않습니다.
-
-    // 로그인 성공시 서비스의 로그인 및 회원가입 처리가 필요하다.
-    // 인증 성공시 서비스의 로그인 처리는 success 콜백 함수를 사용해야 한다.
-
     success: function (authobj) {
-      // 로그인이 성공할 경우 토큰을 받을 콜백함수 success
-      // console.log(authobj);
-
+      // http://10.58.4.105:8000/users
       fetch('http://10.58.4.105:8000/users', {
         method: 'POST',
-        body: JSON.stringify({
-          token: authobj.access_token, // 키 값 맞춰야함
-        }),
-      }).then(res => res.json());
-
-      // .catch(error => console.log(error));
-      // .then(data =>
-      //   data.message === 'SUCCESS'
-      //     ? localStorage.setItem('token', data.token)
-      //     : console.log(data.message)
-      // )
+        headers: {
+          Authorization: authobj.access_token,
+        },
+      })
+        .then(res => res.json())
+        .then(data =>
+          data.message === 'SUCCESS'
+            ? sessionStorage.setItem('hines_token', data.Authorization)
+            : alert(data.message)
+        );
     },
   });
 };
